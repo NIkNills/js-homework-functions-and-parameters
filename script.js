32a54a942d3a20acb6655f598875062f1a5b1bec
Nik Kolesnikov <niknills94@gmail.com>
2022-08-26 12:03:41 +0300

    feat: added task 1 solution

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const helloWorld = "Hello world";
+
+const user = {
+  name: "John",
+  surname: "Doe",
+  age: 28,
+};
+
+user.skill = "React";
+user.gender = "male";
+
+user.sayHelloWorld = () => helloWorld;
+user.sayHi = function () {
+  return `${this.name} ${this.surname} says Hi`;
+};
